7ff429826c00751cc87fb01960eaeb27df332431
Chi Dung <Dungnc0402@gmail.com>
2022-05-27 23:19:20 +0700

    update router

---
+++ pages/posts/create.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type PropsCreatePostPage = {}
+
+const CreatePostPage = (props: PropsCreatePostPage) => {
+  return (
+    <div>CreatePostPage</div>
+  )
+}
+
+export default CreatePostPage
---
+++ pages/posts/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type PropsPostPropsPage = {}
+
+const PostPropsPage = (props: PropsPostPropsPage) => {
+  return (
+    <div>PostPropsPage</div>
+  )
+}
+
+export default PostPropsPage
